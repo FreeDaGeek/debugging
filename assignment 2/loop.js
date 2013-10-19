d96ce3887780818c80b153afc2b0d5019f23ddc3
FreeDaGeek <free.rosas@gmail.com>
2013-10-19 18:50:32 -0400

    Added a few new things

---
+++ assignment 2/loop.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Frosas32 on 10/19/13.
+ */
+console.log('-----Loops-----');
+
+var b = 10 //setup the index
+
+while ( b > 0){ // checks the condition
+    console.log(b);
+    b--; //increments or decrements the index
+}
+
+//While Loop ^^^^
+
+console.log('----Do While Loops-----');
+
+var c = 10;
+do{
+    console.log(c +' kegs on the wall');
+    c--;
+
+}
+while (c >1);
