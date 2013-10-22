b6916fd1eb367eb775f9b03dd8134d2d2018eb8b
FreeDaGeek <free.rosas@gmail.com>
2013-10-22 12:00:16 -0400

    Last update

--- assignment 2/loop.js
+++ assignment 2/loop.js
@@ -21,3 +21,18 @@ do{
 
 }
 while (c >1);
+
+
+
+//Functions
+
+var width = 5;
+
+function calArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+console.log (width);
+calArea();
