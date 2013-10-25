ba9ee0bbd792ed2d1d236d43fae489a3694bb1e3
FreeDaGeek <free.rosas@gmail.com>
2013-10-25 11:14:20 -0400

    completed the assignment / late tho.

--- assignment 2/loop.js
+++ assignment 2/loop.js
@@ -2,37 +2,27 @@
  * Created by Frosas32 on 10/19/13.
  */
 console.log('-----Loops-----');
-
 var b = 10 //setup the index
-
-while ( b > 0){ // checks the condition
+while (b > 0) { // checks the condition
     console.log(b);
     b--; //increments or decrements the index
 }
-
 //While Loop ^^^^
-
 console.log('----Do While Loops-----');
-
 var c = 10;
-do{
-    console.log(c +' kegs on the wall');
+do {
+    console.log(c + ' kegs on the wall');
     c--;
-
 }
-while (c >1);
-
-
-
+while (c > 1);
 //Functions
-
 var width = 5;
 
-function calArea(){
+function calArea() {
     var width = 20;
     var height = 30;
     var area = width * height;
     console.log(area);
 }
-console.log (width);
+console.log(width);
 calArea();
